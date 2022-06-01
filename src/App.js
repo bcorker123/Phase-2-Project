639ca1575e08da48f7f6146d9ff46459e8a46a00
brian corker <bcorker123@gmail.com>
2022-06-01 09:34:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// 1. You must make a single page application (only one index.html file)
+// using create-react-app.
+
+// 2. Your app should use at least 5 components in a way that keeps your
+// code well organized.
+
+// 3. There should be at least 3 client-side routes using React Router
+// Be sure to include a nav bar or other UI element that allows users to
+// navigate between routes.
+
+// 4. Use a json-server to create a RESTful API for your backend and
+// make both a GET and a POST request to the json server. Use a form
+// to make your post request, specifically a controlled form/component.
+// Additionally, you may choose to incorporate data from an external API
+// but it is not required. You should keep your json-server data simple:
+// avoid nested data and associations. You'll learn how to work with
+// more complex data in the next two phases. Focus on the frontend for
+// this project.
+
+// 5. Add some styling: you're encouraged to write your CSS from scratch,
+// either by using styled components or writing CSS files and using
+// id/className to style your elements. You can also incorporate a UI
+// framework (like React Bootstrap, Semantic UI , or Material UI) if
+// you prefer.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">phase 2 project</div>;
 }
 
 export default App;
